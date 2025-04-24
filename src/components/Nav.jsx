d8507c7330aff16f4fe18e6f21eac7f5b33a230b
Sumit Kumar Sharma <77589969+SumitLabs@users.noreply.github.com>
2025-04-24 23:30:59 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../utility.css";
 import "./nav.css";
 import { TiTimes, TiThMenu } from "react-icons/ti";
@@ -6,14 +6,31 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   const [mobile, setMobile] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
   
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   const toggleHamburger = () => {
     setMobile(!mobile);
   };
 
   return (
-    <nav className="flex_box justify_between align_center">
-      <div className="logo"><h3>Sumit Kumar Sharma</h3></div>
+    <nav className={`flex_box justify_between align_center ${scrolled ? 'scrolled' : ''}`}>
+      <div className="logo"><h3> <span>&lt;</span> Sumit Kumar Sharma<span>&nbsp;&#8725;&nbsp;&gt;</span></h3></div>
       <ul className={`flex_box flex_gap my-16 nav_links ${mobile ? 'active' : ''}`}>
         <li>
           <Link to="/" onClick={() => setMobile(false)}>Home</Link>
